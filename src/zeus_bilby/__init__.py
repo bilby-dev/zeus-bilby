aae167ac5e94a2a9f5bf2bd219da3643112e6aa9
Michael J. Williams <michaeljw1@googlemail.com>
2026-02-11 17:56:53 +0000

    MAINT: copy plugin from bilby

---
+++ src/zeus_bilby/__init__.py
@@ -0,0 +1,13 @@
+"""Interface and plugin for using zeus(-mcmc) in bilby."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+from .plugin import Zeus
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "unknown"
+
+__all__ = ["Zeus"]
